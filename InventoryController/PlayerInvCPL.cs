e2fd3956e9a37448a46bc03c2b72401bc10f2ee4
SagaeHaruki_ <92028294+SagaeHaruki@users.noreply.github.com>
2024-02-10 22:12:57 +0900

    Add files via upload

--- InventoryController/PlayerInvCPL.cs
+++ InventoryController/PlayerInvCPL.cs
@@ -2,19 +2,25 @@ using Cinemachine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInvCPL : MonoBehaviour
 {
     Movement playerMovement;
 
     [SerializeField] public GameObject InventoryGUI;
+    [SerializeField] private InventoryManager InventoryScript;
 
     private void Awake()
     {
         playerMovement = GetComponent<Movement>();
+        InventoryScript = InventoryGUI.GetComponent<InventoryManager>();
         InventoryGUI.SetActive(false);
+
+        InventoryScript.DeselectAllSlots();
     }
 
     private void Update()
@@ -27,11 +33,15 @@ public class PlayerInvCPL : MonoBehaviour
             {
                 InventoryGUI.SetActive(true);
                 playerMovement.onInventory = true;
+                InventoryScript.DeselectAllSlots();
+                InventoryScript.ResetItemDesc();
             }
             else if (Input.GetKeyDown(KeyCode.B) && playerMovement.onInventory)
             {
                 InventoryGUI.SetActive(false);
                 playerMovement.onInventory = false;
+                InventoryScript.DeselectAllSlots();
+                InventoryScript.ResetItemDesc();
             }
         }
     }
